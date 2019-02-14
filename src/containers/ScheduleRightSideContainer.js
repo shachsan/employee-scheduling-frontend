77d1ba92b9ec0e0e976e2b9ac9d1babb1a13b49f
Sanjay Sai <shachsan@hotmail.com>
2019-02-13 22:06:35 -0500

    apply css

--- src/containers/ScheduleRightSideContainer.js
+++ src/containers/ScheduleRightSideContainer.js
@@ -12,9 +12,9 @@ class ScheduleRightSideContainer extends Component{
             console.log('rendered right container', evnts);
         return evnts.map(evnt=>(
             <div key={evnt.id}>
-                    <span>{evnt.event_date} | {dateFns.format(evnt.event_date, 'ddd')}</span>
-                    <h4 style={{display:'inline'}}>=></h4>
-                    <span>{evnt.event}</span>
+                    <div style={{display:'inline-block', marginRight:'10px'}}><span>{evnt.event_date} | {dateFns.format(evnt.event_date, 'ddd') } </span></div>
+                    {/* <div style={{display:'inline-block'}}></div> */}
+                    <div style={{display:'inline-block'}}><span> - {evnt.event}</span></div>
                 </div>))
         
     }
@@ -22,7 +22,8 @@ class ScheduleRightSideContainer extends Component{
     render(){
         return ( 
             <div className='side-container'> 
-                <h2>Plan your schedules based on the events</h2>
+                <div style={{textAlign:'center', marginBottom:'15px', borderBottom:'1px solid orange'}}><h2>Plan your schedules based on the events</h2></div>
+                
                 {this.renderEvents()}
                 {/* <AnimationDiv/> */}
             </div>
