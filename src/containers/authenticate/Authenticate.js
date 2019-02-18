1cfcdc4da770a49e97ba3ca6579cd7911db6874c
Sanjay Sai <shachsan@hotmail.com>
2019-02-18 12:55:28 -0500

    login credential for testing app

--- src/containers/authenticate/Authenticate.js
+++ src/containers/authenticate/Authenticate.js
@@ -35,6 +35,8 @@ class Authenticate extends Component {
                     <form className="login" onSubmit={this.onSubmitHandler}>
                         <div style={{paddingTop:'15px', height:'50px', margin:'0px', backgroundColor:'yellowgreen', outline:'none', textAlign:'center'}}>
                         <h3 style={{color:'midnightblue'}}>Welcome</h3></div>
+                        <div style={{backgroundColor:'yellow', color:'blue'}}>To test this app, please login with the following credentials:</div>
+                        <div style={{backgroundColor:'yellow', color:'blue'}}>username: <span style={{backgroundColor:'green', color:'white', padding:'0 5px'}}>sanjay</span>  password:<span style={{backgroundColor:'green', color:'white', padding:'0 5px', marginLeft:'10px'}}>test</span></div>
                         {this.props.currentUser.message !==null ?
                             <><span style={{backgroundColor:'red', color:'white'}}>{this.props.currentUser.message}</span><br/></>
                         :null}
